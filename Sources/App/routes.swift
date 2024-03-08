3aeda323b6626cfc385cc8ef8336b273d9dd8b5e
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-03-07 21:11:29 -0600

    initial commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,16 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-app.get { req async in
-        "It works!"
+    app.get { req async in
+        "Vapor is running!"
     }
 
     app.get("hello") { req async -> String in
         "Hello, world!"
+    }
+    
+    app.get("hello", ":name") { req async -> String in
+        let name = req.parameters.get("name")
+        return "Hello \(name ?? "Biff")!"
     }
 }
